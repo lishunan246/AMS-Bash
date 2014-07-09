ef6d4ca3ac63b6622ce4ae5bd8ae2b2a40df233d
Nathan <lishunan246@gmail.com>
2014-07-09 13:57:21 +0800

    CountDir.sh Written

--- CountDir.sh
+++ CountDir.sh
@@ -1,2 +1,20 @@
-echo "Directory: $1"
+if [[ $# -ne 1 ]]; then
+	echo "Can not have more than one argument!"
+	exit 1
+fi
+if [[ -d "$1" ]]; then
+	echo "Directory: $1"
+else
+	echo "Directory: $1 does not exists!"
+	exit 1
+fi
+echo "Total size:"
+du -sh $1|cut -f 1
+echo "Number of regular file(s) in the directory:"
+find $1 -type f|wc -l)
+echo "Number of directory(s) in the directory:"
+find $1 -type d|wc -l
+echo "Number of executable file(s) in the directory:"
+find $1 -executable|wc -l
+
 exit 0
