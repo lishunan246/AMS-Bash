3e10251472a79816f23265d9454813f90134e845
Nathan <lishunan246@gmail.com>
2014-07-09 14:02:12 +0800

    Fixed

--- CountDir.sh
+++ CountDir.sh
@@ -11,7 +11,7 @@ fi
 echo "Total size:"
 du -sh $1|cut -f 1
 echo "Number of regular file(s) in the directory:"
-find $1 -type f|wc -l)
+find $1 -type f|wc -l
 echo "Number of directory(s) in the directory:"
 find $1 -type d|wc -l
 echo "Number of executable file(s) in the directory:"
