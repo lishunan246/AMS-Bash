e436ff4d259e3980364f25db7d58c8c4203d7cb2
Nathan <lishunan246@gmail.com>
2014-07-09 10:09:24 +0800

    Test Git on Linux

---
+++ 1.sh
@@ -0,0 +1,47 @@
+if [[ $f "$1" ]]; then
+	#statements
+	echo "fff"
+else
+	echo "dd"
+fi
+
+if [[ 1=1 ]]; then
+	#statements
+	echo 1;
+elif [[ df ]]; then
+		#statements
+		ls
+fi
+
+for (( i = 0; i < 10; i++ )); do
+	#statements
+	echo i
+done
+
+for i in words; do
+	#statements
+done
+
+while [[ hhh ]]; do
+	#statements
+done
+
+until [[ dcdc ]]; do
+	#statements
+done
+
+case word in
+	pattern ) dfdf
+ 		;;
+ 		df )
+	;;
+esac
+
+name=()
+
+fun()
+{
+	df
+}
+
+fun
