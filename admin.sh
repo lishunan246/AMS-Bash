7dd210304fdf95071c9019c3ad22fe0834e2b6be
lishunan246 <lishunan246@gmail.com>
2014-07-21 18:05:13 +0800

    admin.sh updated

--- admin.sh
+++ admin.sh
@@ -3,13 +3,17 @@
 
 #! /bin/bash
 
+#function to create a teacher account
 addTeacher()
 {
 	echo "You are going to add a teacher account"
 	echo "Input teacher ID:"
 	read tid
+
+	#ID can only use a-z A-Z and number, other characters will be ignored
 	tid=$(echo $tid | sed 's/[^a-zA-Z0-9]*//g')
 	echo "Input teacher's name:"
+	#the same as ID
 	read tname
 	tname=$(echo $tname | sed 's/[^a-zA-Z0-9]*//g')
 
@@ -21,6 +25,7 @@ addTeacher()
 		read x
 		case "$x" in
 			y )
+			#add a new line of record in the file teachers
 			printf "$tid $tname\n">>teachers 
 			echo "OK"
 			sleep 1
@@ -32,6 +37,56 @@ addTeacher()
 	done
 }
 
+#function to list all teachers
+listTeacher()
+{
+	echo "ID Name"
+	echo "---------------"
+	cat teachers
+	echo "---------------"
+	echo "Enter q to quit"
+
+	while [[ true ]]; do
+		read x
+		case "$x" in
+			q) clear 
+			return 1;;
+			
+			*);;
+		esac
+	done
+}
+
+#function to delete a teacher account
+deleteTeacher()
+{
+	echo "You are going to delete a teacher account"
+	echo "Input teacher ID:"
+	read tid
+
+	#ID can only use a-z A-Z and number, other characters will be ignored
+	tid=$(echo $tid | sed 's/[^a-zA-Z0-9]*//g')
+	
+
+
+	while [[ true ]]; do
+		echo "ID: "$tid
+		echo "Delete the account? (y/n)"
+		read x
+		case "$x" in
+			y )
+			#remove the line from teachers
+			#sed "/^\"$tid\" /d" teachers
+			sed "/^$tid /d" teachers>teachers
+			echo "OK"
+			sleep 1
+			return	1;;
+			n ) return 0;;
+			*) echo "Answer y or n"
+		esac
+		sleep 1
+	done
+}
 
 while [[ true ]]; do
 	clear
@@ -49,7 +104,8 @@ while [[ true ]]; do
 	case $num in
 		1) addTeacher;;
 		2) bash teacher.sh;;
-		3) bash student.sh;;
+		3) deleteTeacher;;
+		4) listTeacher;;
 		0) clear
 		
 		sleep 1
